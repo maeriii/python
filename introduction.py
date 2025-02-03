2f2a100d07f6aa575e96cf70c9f12c5bd6d0f3c0
maeriii <mwanziaedwin5@gmail.com>
2025-02-03 13:22:05 +0300

    First commit

---
+++ introduction.py
@@ -0,0 +1,5 @@
+#print is a method used to display a value
+print("maeriii")
+print("emobilis")
+print(39)
+print(100*2)
---
+++ variable.py
@@ -0,0 +1,22 @@
+#variable is a container used to store a value
+
+number = 10
+firstname = "Maeri"
+lastname = "eMobilis"
+language = "Python"
+a = 10
+b= 15
+
+print(number)
+print(firstname)
+print(lastname)
+print(language, "is a web programming language")
+print(a + b)
+
+#The sum of 10 and 15 is 25
+
+x = 10
+y = 15
+Z = x + y
+
+print("The sum of" ,x,"and" ,y,"is",Z)
